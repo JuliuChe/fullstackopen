0e1ef0a37148fb55c36ed5dc382bcae1c2bff417
Julien Chevalley <chevalleyjulien2@gmail.com>
2026-03-12 16:36:00 +0100

    part 3.15-3.18 remove .env from git

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -149,6 +149,7 @@ const errorHandler = (error, request, response, next) => {
   if(error.name === "CastError"){
     return response.status(400).send({error:'malformatted id'})
   }
+  next(error)
 }
 app.use(errorHandler)
 
