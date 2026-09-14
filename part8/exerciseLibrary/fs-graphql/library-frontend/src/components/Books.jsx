a61aab2e63a600b32c48778f41a7fe1b26f92079
Julien Chevalley <chevalleyjulien2@gmail.com>
2026-09-14 17:41:13 +0200

    exercise20-Books by genre, part1

--- part8/exerciseLibrary/fs-graphql/library-frontend/src/components/Books.jsx
+++ part8/exerciseLibrary/fs-graphql/library-frontend/src/components/Books.jsx
@@ -1,8 +1,11 @@
 import { ALL_BOOKS } from '../queries'
 import { useQuery } from '@apollo/client/react'
+import { useState } from 'react'
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  const [selectedGenres, setGenreSelection] = useState([])
+
   if (!props.show) {
     return null
   }
@@ -10,12 +13,33 @@ const Books = (props) => {
     return <div>loading...</div>
   }
 
+  const addGenre = (id) => {
+    if (selectedGenres.includes(id)) {
+      setGenreSelection(selectedGenres.filter((g) => g !== id))
+    } else {
+      setGenreSelection(selectedGenres.concat(id))
+    }
+  }
+
   const books = result.data.allBooks
+  const genres = [...new Set(books.flatMap((b) => b.genres)), 'all genres']
+  console.log(selectedGenres)
+
+  const filteredBooks =
+    selectedGenres.length === 0 || selectedGenres.includes('all genres')
+      ? books
+      : books.filter((book) =>
+          book.genres.some((genre) => selectedGenres.includes(genre)),
+        )
 
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        {' '}
+        in {selectedGenres.length > 0 ? '' : 'all'} genres{' '}
+        {selectedGenres.length > 0 ? selectedGenres.join(', ') : ''}
+      </div>
       <table>
         <tbody>
           <tr>
@@ -23,15 +47,36 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {filteredBooks.map((a) => {
+            return (
+              <tr key={a.id}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )
+          })}
+
+          {/* // }
+          // )} */}
         </tbody>
       </table>
+      <div>
+        {genres.map((g) => {
+          const isSelected = selectedGenres.includes(g)
+          return (
+            <button
+              key={g}
+              onClick={() => addGenre(g)}
+              style={{
+                border: isSelected ? '3px solid #2388cc' : '1px solid #86888a',
+              }}
+            >
+              {g}
+            </button>
+          )
+        })}
+      </div>
     </div>
   )
 }
